ca1c3321e5f06bb93b9f7aac8745ad3da8a1fd73
KennethMelendez <kmlndz221@gmail.com>
2022-02-17 22:02:37 -0500

    building out dynamodb mapper and package classes

---
+++ Services/DynamoDBAdvertStorage.cs
@@ -0,0 +1,41 @@
+﻿using AdvertApi.Models;
+using System.Threading.Tasks;
+using AutoMapper;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using System;
+
+namespace AdvertApi.Services
+{
+    public class DynamoDBAdvertStorage : IAdvertStorageService
+    {
+        private readonly IMapper _mapper;
+
+        public DynamoDBAdvertStorage(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<string> Add(AdvertModel model)
+        {
+            var dbModel = _mapper.Map<AdvertDbModel>(model);
+            dbModel.Id = new Guid().ToString();
+            dbModel.CreationDateTime = DateTime.Now;
+            dbModel.Status = AdvertStatus.Pending;
+
+            using (var client = new AmazonDynamoDBClient())
+            {
+                using (var context = new DynamoDBContext(client))
+                {
+                    await context.SaveAsync(dbModel);
+                }
+            }
+            return dbModel.Id;
+        }
+
+        Task<bool> IAdvertStorageService.Confirm(ConfirmAdvertModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
